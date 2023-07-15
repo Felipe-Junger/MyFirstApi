075c959ef2865265e0a01f719cb117dad2d394ff
Felipe <felipejunger7@gmail.com>
2023-07-15 13:42:10 -0300

    criando parâmetros

--- src/server.js
+++ src/server.js
@@ -2,5 +2,20 @@ const express = require('express');
 
 const app = express();
 
+app.get("/message/:id/:user", (request, response) => {
+   const { id, user } = request.params;
+   
+    response.send(`
+        Message ID: ${id}.
+        Para o usuário: ${user}.
+        `);
+    });
+
+app.get("/users", (request, response) => {
+    const { pages, limit } = request.query;
+
+    response.send(`Pages ${pages}, Limit ${limit}`);
+});
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
