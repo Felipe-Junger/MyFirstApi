057556685a8d8b345e9a8c51d00422da34d7ee0e
Felipe <felipejunger7@gmail.com>
2023-07-16 18:23:34 -0300

    Trantando exceções - Erro

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
+require ('express-astync-errors');
+
+const AppError = require('./utils/AppError');
+
 const express = require('express');
 
 const routes = require("./routes");
@@ -7,6 +11,19 @@ app.use(express.json());
 
 app.use(routes);
 
+app.use(( error, request, responde, next ) => {
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: 'error',
+            message: error.message
+        });
+    }
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+    });
+});
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
