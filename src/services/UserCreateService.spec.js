9a6b4e7381a1bd952adf1652e1213c2f0570f513
Felipe <felipejunger7@gmail.com>
2023-08-08 19:24:14 -0300

    inserindo o describe

--- src/services/UserCreateService.spec.js
+++ src/services/UserCreateService.spec.js
@@ -1,16 +1,19 @@
 const UserCreateService = require('./UserCreateService');
 const UserRepositoryinMemory = require("../repositories/UserRepositoryinMemory");
 
-it("user should be create", async () => {
-    const user = {
-        name: 'User test',
-        email: "user@test.com",
-        password: "123"
-    };
+describe("UserCreateService", () => {
+    it("user should be create", async () => {
+        const user = {
+            name: 'User test',
+            email: "user@test.com",
+            password: "123"
+        };
+    
+        const UserRepositoryinMemory = new UserRepositoryinMemory();
+        const UserCreateService = new UserCreateService(UserRepositoryinMemory);
+        const userCreated = await UserCreateService.execute(user);
+    
+        expect(userCreated).toHaveProperty("id");
+    });
+})
 
-    const UserRepositoryinMemory = new UserRepositoryinMemory();
-    const UserCreateService = new UserCreateService(UserRepositoryinMemory);
-    const userCreated = await UserCreateService.execute(user);
-
-    expect(userCreated).toHaveProperty("id");
-});
